1af5d7b2c66f22cbfa329071c1ad69b5e7a212dd
Jaime Pérez <jaipecas89@gmail.com>
2026-09-12 17:09:49 +0200

    sec14-DDD abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggretate.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Ordering.Domain.Abstractions;
+
+public interface IAggregate<T>: IAggretate, IEntity<T>
+{
+
+}
+
+public interface IAggretate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
+
